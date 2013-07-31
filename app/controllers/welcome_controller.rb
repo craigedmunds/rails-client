0e7a7255f5ff960f6e25412a97290c03221cd881
Craig Edmunds <craig.edmunds@gmail.com>
2013-07-31 17:43:31 +0100

    Handle issue on welcome screen when user not authenticated

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,18 @@
 class WelcomeController < ApplicationController
   def index
-  	@consumer_tokens=TestServiceToken.all :conditions=>{:user_id=>current_user.id}
-    
+  	
+  	if current_user
+	  	@consumer_tokens=TestServiceToken.all :conditions=>{:user_id=>current_user.id}
+	    
 
-    @token = @consumer_tokens.first
+	    @token = @consumer_tokens.first
 
-    if @token
-    	@client = @token.client
-    	@data = @client.get("/api/v2/users").body
+	    if @token
+	    	@client = @token.client
+	    	@data = @client.get("/api/v2/users").body
 
-	  	logger.info "private data: " + @data
-	  end
+		  	logger.info "private data: " + @data
+		  end
+		end
   end
 end
