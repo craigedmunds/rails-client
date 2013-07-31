241c17ac0a68bfc646944dfb83831bc9fb05cf42
Craig Edmunds <craig.edmunds@gmail.com>
2013-07-31 17:41:51 +0100

    Outputting API data to welcome screen if present

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,15 @@
 class WelcomeController < ApplicationController
   def index
   	@consumer_tokens=TestServiceToken.all :conditions=>{:user_id=>current_user.id}
-    @token = @consumer_tokens.first.client
-  	logger.info "private data: " + @token.get("/api/v2/users").body
+    
+
+    @token = @consumer_tokens.first
+
+    if @token
+    	@client = @token.client
+    	@data = @client.get("/api/v2/users").body
+
+	  	logger.info "private data: " + @data
+	  end
   end
 end
